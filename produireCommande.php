6aa06338cd3e0e39c2c010c7a212c272217eb709
AngeSilue <fsilac99@gmail.com>
2019-05-01 03:55:44 -0300

    Ajout deuxieme partie (Validation et affichage) et ajout balise fermeture html

--- produireCommande.php
+++ produireCommande.php
@@ -35,5 +35,37 @@
     </head>
 
 <body>
+<h1>Ange PiÃ¨ces d'auto</h1>
+        <hr />
+        <br />
+        <?php
+        echo '<h2>Commande produite Ã  ' . date('G:i \l\e d-m-y ') . '</h2><br />' . "\n";
 
+        if (isset($_POST['txtPneux']) && is_numeric($_POST['txtPneux'])) {
+            $nbPneux = $_POST['txtPneux'];
+        }
+
+        if (isset($_POST['txtHuile']) && is_numeric($_POST['txtHuile'])) {
+            $nbHuiles = $_POST['txtHuile'];
+        }
+
+        if (isset($_POST['txtBougies']) && is_numeric($_POST['txtBougies'])) {
+            $nbBougies = $_POST['txtBougies'];
+        }
+
+
+        //calcul
+        $totalItem = $nbBougies + $nbPneux + $nbHuiles;
+        $prixTotal = $nbBougies * PRIX_BOUGIES + $nbPneux * PRIX_PNEUX + $nbHuiles * PRIX_HUILES;
+
+        //sortie
+        echo "<ul><li>Nombre total d'items : $totalItem </li>\n";
+        echo "<li>DÃ©tails de la commande</li>\n";
+        echo "<ol><li>Pneux: $nbPneux </li>\n";
+        echo "<li>Huile: $nbHuiles </li>\n";
+        echo "<li>Bougies: $nbBougies </li></ol>\n";
+        echo "<li>Prix Total :$" . number_format($prixTotal, 2) . "</li></ul>";
+        ?>
 </body>
+
+</html>
