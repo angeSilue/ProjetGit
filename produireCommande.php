1e1c3a218ba7face1c8f4d30aeba7bedb91a49cd
AngeSilue <fsilac99@gmail.com>
2019-05-01 03:51:49 -0300

    Ajout premiere partie (declaration des variables)

--- produireCommande.php
+++ produireCommande.php
@@ -3,3 +3,37 @@
 	date 	: Mai 2019
 	but 	: Faire afficher une facture de pieces autos 
 -->
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" >
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>Commande</title>
+        <style type="text/css">
+
+            h1 {
+                text-align : center;
+
+            }
+
+            body
+            {
+                background-color : silver;
+            }
+
+        </style>
+        <?php
+        define("PRIX_PNEUX", 100);
+        define("PRIX_HUILES", 10);
+        define("PRIX_BOUGIES", 4);
+
+        $nbPneux = 0;
+        $nbHuiles = 0;
+        $nbBougies = 0;
+        ?>
+    </head>
+
+<body>
+
+</body>
